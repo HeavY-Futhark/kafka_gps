0e5faac077198217e6dfe8cfaec59998bddd366a
Arthur Klein <kleinarthu@cy-tech.fr>
2023-11-29 16:32:34 +0100

    Added a basic app with leaflet

---
+++ frontend/my-app/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Map from "./Map.svelte";
+</script>
+
+<main>
+  <h1>Coordinate</h1>
+  <Map />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    margin: 1em;
+  }
+</style>
+
---
+++ frontend/my-app/src/Map.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+  import "leaflet/dist/leaflet.css";
+  import L from "leaflet"
+
+  onMount(() => {
+    const map = L.map("map").setView([51.505, -0.09], 13);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+  });
+</script>
+
+<style>
+  #map {
+    height: 400px;
+    width: 100%;
+  }
+</style>
+
+<div id="map"></div>
+
